b4c286ee697b354c7314cb94b54e7bfa8aaf6281
Norbert Yemuang <66695064+Bope142@users.noreply.github.com>
2024-02-16 07:34:59 +0100

    add moving text

--- app/page.jsx
+++ app/page.jsx
@@ -2,25 +2,24 @@ import { CardCategory, CardPostSimple } from "@/components/cards/Cards";
 import "../public/style/main.scss";
 import "./home.style.scss";
 import TitleSection from "@/components/titleSection/TitleSection";
-
+const categories = [
+  { title: "Technologie et Innovation", cover: "/images/tech_cover.png" },
+  { title: "Voyage et Aventure", cover: "/images/voyage.jpg" },
+  { title: "Cuisine et Gastronomie", cover: "/images/Cuisine.jpg" },
+  { title: "Art et Culture", cover: "/images/Art.jpg" },
+  { title: "Santé et Bien-être", cover: "/images/Sante.jpg" },
+  { title: "Mode et Beauté", cover: "/images/Mode.jpg" },
+  { title: "Finance et Investissement", cover: "/images/Finance.jpg" },
+  {
+    title: "Environnement et Durabilité",
+    cover: "/images/Environnement.jpg",
+  },
+  { title: "Parentalité et Éducation", cover: "/images/Education.jpg" },
+  { title: "Science et Nature", cover: "/images/Science.jpg" },
+  { title: "Sports et Fitness", cover: "/images/Sports.jpg" },
+  { title: "Actualités et Politique", cover: "/images/Politique.jpg" },
+];
 const CategoriePostSection = () => {
-  const categories = [
-    { title: "Technologie et Innovation", cover: "/images/tech_cover.png" },
-    { title: "Voyage et Aventure", cover: "/images/voyage.jpg" },
-    { title: "Cuisine et Gastronomie", cover: "/images/Cuisine.jpg" },
-    { title: "Art et Culture", cover: "/images/Art.jpg" },
-    { title: "Santé et Bien-être", cover: "/images/Sante.jpg" },
-    { title: "Mode et Beauté", cover: "/images/Mode.jpg" },
-    { title: "Finance et Investissement", cover: "/images/Finance.jpg" },
-    {
-      title: "Environnement et Durabilité",
-      cover: "/images/Environnement.jpg",
-    },
-    { title: "Parentalité et Éducation", cover: "/images/Education.jpg" },
-    { title: "Science et Nature", cover: "/images/Science.jpg" },
-    { title: "Sports et Fitness", cover: "/images/Sports.jpg" },
-    { title: "Actualités et Politique", cover: "/images/Politique.jpg" },
-  ];
   return (
     <section className="section_page content__categorie_post">
       {categories.map((category, index) => (
@@ -163,11 +162,24 @@ const BestPostSection = () => {
     </section>
   );
 };
+
+const SectionCategory = () => {
+  return (
+    <div className="categories__post marquee">
+      <div className="marquee-content">
+        {categories.map((category, index) => (
+          <span key={index}>{category.title} - </span>
+        ))}
+      </div>
+    </div>
+  );
+};
 export default function Home() {
   return (
     <main className="page__content" id="homePage">
       <CategoriePostSection />
       <BestPostSection />
+      <SectionCategory />
     </main>
   );
 }
