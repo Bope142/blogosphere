de26b857551562ccf264ef35d92744a95a238eb7
Norbert Yemuang <66695064+Bope142@users.noreply.github.com>
2024-03-12 16:24:37 +0100

    can show available categorie in page categorie

--- app/categories/page.jsx
+++ app/categories/page.jsx
@@ -1,35 +1,38 @@
+"use client";
+import { useGetCategories } from "@/hooks/useCategorie";
 import "../../public/style/main.scss";
 import "./style.scss";
 import { CardCategory } from "@/components/cards/Cards";
 export default function CategoriePage() {
-  const categories = [
-    { title: "Technologie et Innovation", cover: "/images/tech_cover.png" },
-    { title: "Voyage et Aventure", cover: "/images/voyage.jpg" },
-    { title: "Cuisine et Gastronomie", cover: "/images/Cuisine.jpg" },
-    { title: "Art et Culture", cover: "/images/Art.jpg" },
-    { title: "Santé et Bien-être", cover: "/images/Sante.jpg" },
-    { title: "Mode et Beauté", cover: "/images/Mode.jpg" },
-    { title: "Finance et Investissement", cover: "/images/Finance.jpg" },
-    {
-      title: "Environnement et Durabilité",
-      cover: "/images/Environnement.jpg",
-    },
-    { title: "Parentalité et Éducation", cover: "/images/Education.jpg" },
-    { title: "Science et Nature", cover: "/images/Science.jpg" },
-    { title: "Sports et Fitness", cover: "/images/Sports.jpg" },
-    { title: "Actualités et Politique", cover: "/images/Politique.jpg" },
-  ];
-  return (
+  const { data: categories, isFetching, isError } = useGetCategories();
+  const display = isFetching ? (
+    <main className="page__content">
+      <section className="section_page categories__list">
+        {[...Array(8)].map((_, index) => (
+          <CardCategory
+            key={index}
+            title={""}
+            cover={""}
+            id={0}
+            loading={true}
+          />
+        ))}
+      </section>
+    </main>
+  ) : (
     <main className="page__content">
       <section className="section_page categories__list">
         {categories.map((category, index) => (
           <CardCategory
             key={index}
-            title={category.title}
-            cover={category.cover}
+            title={category.name_categorie}
+            cover={category.coverPath}
+            id={category.category_id}
+            loading={false}
           />
         ))}
       </section>
     </main>
   );
+  return display;
 }
